17074c7267c663750be094d19e3e21e0d313dd3c
chris <chris@aion.network>
2019-01-14 14:53:47 +0800

    add multi-langs support

--- components/unidentified/password.js
+++ components/unidentified/password.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
+import langs from '../../langs.js';
 
 class Password extends Component {
-	render(){
+	constructor(props){
+		super(props);
+		this.state = {
+			password: ''
+		}
+	}
+	componentDidMount(){
 		console.log('[route] ' + this.props.navigation.state.key);
 		console.log(this.props.user);
+	}
+	render(){
 		return (
-			<View>
-				<Text>Input Password {JSON.stringify(this.props)}</Text>
-
+			<View style={{
+		        flex: 1,
+        		flexDirection: 'column',
+        		justifyContent: 'center',
+        		alignItems: 'center',
+			}}>
+				<Text>{langs.enter_password.cn}</Text>
+				<TextInput
+			        style={{
+			        	width: 240,
+			        	height: 40,
+			        	borderBottomWidth: 1, 
+			        	borderColor: '#adb0b5',
+			        	alignSelf: 'center',
+			        }}
+			        onChangeText={(password) => this.setState({
+			        	password
+			        })}
+			        secureTextEntry={true}
+			        value={this.state.password}
+			    />
 			</View>
 		);
 	}
---
+++ langs.js
@@ -0,0 +1,6 @@
+export default langs = {
+	enter_password: {
+		en: 'Enter password',
+		cn: '输入密码'
+	}
+};
