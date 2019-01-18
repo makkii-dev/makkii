6e3a911a7a5cbc5f74595f89b74501547ebbed71
chris <chris@aion.network>
2019-01-19 01:59:44 +0800

    refactor: move test data to data.js

--- App.js
+++ App.js
@@ -33,27 +33,10 @@ const store = createStore(combineReducers({
 	user:     reducer_user,
 }));
 
-// temp load some demo data
-import Account from './types/account.js';
-let _accounts = {};
-let length = 30;
-for(let i = 0; i < 20; i++){
-	let hex = Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
-	let acc = new Account();
-	acc.address = 'a0' + hex;
-	acc.private_key = '0000000000000000000000000000000000000000000000000000000000000000';
-	acc.balance = Math.random().toFixed(6);
-	acc.name = 'account ' + i;
-	acc.type = '[local]'; 
-	_accounts[acc.address] = acc;
-}
-store.dispatch(accounts(_accounts));
-import Dapp from './types/dapp.js';
-let _dapps = [];
-_dapps.push(new Dapp('dapp1', 'https://i0.wp.com/www.blockchaindk.com/wp-content/uploads/2017/11/Aion-Logo.png?fit=400%2C400&ssl=1', 'desc1'));
-_dapps.push(new Dapp('dapp2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDHgU12UjzCm0m-96M-nsY3gpNuwCl9-dLlmmzlEvsEdHEtJt9', 'desc2'));
-store.dispatch(dapps(_dapps));
-// end temp
+// dummy data
+import data from './data.js';
+store.dispatch(accounts(data.accounts));
+store.dispatch(dapps(data.dapps));
 
 const Container = createAppContainer(createSwitchNavigator({
 	Test: { screen: Test },
---
+++ data.js
@@ -0,0 +1,39 @@
+import Account from './types/account.js';
+import Transaction from './types/transaction.js';
+import Dapp from './types/dapp.js';
+
+let accounts = {};
+let length = 30;
+for(let i = 0; i < 20; i++){
+	let acc = new Account();
+	acc.address = 'a0' + Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
+	acc.private_key = '0000000000000000000000000000000000000000000000000000000000000000';
+	acc.balance = Math.random().toFixed(6);
+	acc.name = 'account ' + i;
+	acc.type = '[local]'; 
+	let tx = new Transaction();
+	tx.hash = Math.round((Math.pow(36, 32) - Math.random() * Math.pow(36, 31))).toString(36).slice(1);
+	tx.from = 'a0' + Math.round((Math.pow(36, 31) - Math.random() * Math.pow(36, 30))).toString(36).slice(1);
+	tx.to = 'a0' + Math.round((Math.pow(36, 31) - Math.random() * Math.pow(36, 30))).toString(36).slice(1);
+	tx.value = Math.random();
+	tx.block = Math.random();
+	acc.transactions[tx.hash] = tx;
+	tx = new Transaction();
+	tx.hash = Math.round((Math.pow(36, 32) - Math.random() * Math.pow(36, 31))).toString(36).slice(1);
+	tx.from = 'a0' + Math.round((Math.pow(36, 31) - Math.random() * Math.pow(36, 30))).toString(36).slice(1);
+	tx.to = 'a0' + Math.round((Math.pow(36, 31) - Math.random() * Math.pow(36, 30))).toString(36).slice(1);
+	tx.value = Math.random();
+	tx.block = Math.random();
+	acc.transactions[tx.hash] = tx;
+	accounts[acc.address] = acc;
+}
+
+
+let dapps = [];
+dapps.push(new Dapp('dapp1', 'https://i0.wp.com/www.blockchaindk.com/wp-content/uploads/2017/11/Aion-Logo.png?fit=400%2C400&ssl=1', 'desc1'));
+dapps.push(new Dapp('dapp2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDHgU12UjzCm0m-96M-nsY3gpNuwCl9-dLlmmzlEvsEdHEtJt9', 'desc2'));
+
+export default data = {
+	accounts,
+	dapps,
+}
