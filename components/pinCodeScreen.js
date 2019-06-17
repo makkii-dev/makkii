571b15052b5d4942c4d3a9906f22aac476808724
Leo Ren <leo.ren@aion.network>
2019-06-17 18:14:16 +0800

    fix error message in pin code screen.

--- components/pinCodeScreen.js
+++ components/pinCodeScreen.js
@@ -222,7 +222,7 @@ class PinCodeScreen extends React.Component {
             .catch(error => {
                 setTimeout(()=>listenApp.ignore = false, 1000);
                 if(error.code!=='USER_CANCELED'&&error.code!=='SYSTEM_CANCELED'){
-                    AppToast.show('Authentication Failed ' + error);
+                    AppToast.show(strings(`pinCode.touchID_${error.code}`));
                 }
             });
     };
