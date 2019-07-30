61627d1017fd7afa019eb323ebf3850dc39016ba
Chengchcc <chen@aion.network>
2019-07-30 15:23:32 +0800

    update eslint

--- libs/aion-web3-inject/webView-invoke/browser.js
+++ libs/aion-web3-inject/webView-invoke/browser.js
@@ -5,42 +5,42 @@ import { createMessager } from './messager/index';
 const isBrowser = typeof window !== 'undefined';
 
 const {
-  bind,
-  define,
-  listener,
-  ready,
-  fn,
-  addEventListener,
-  removeEventListener,
-  isConnect,
+    bind,
+    define,
+    listener,
+    ready,
+    fn,
+    addEventListener,
+    removeEventListener,
+    isConnect,
 } = createMessager((data: any) => {
-  isBrowser &&
-    window.ReactNativeWebView &&
-    window.ReactNativeWebView.postMessage(JSON.stringify(data));
+    isBrowser &&
+        window.ReactNativeWebView &&
+        window.ReactNativeWebView.postMessage(JSON.stringify(data));
 });
 
 if (isBrowser) {
-  let originalPostMessage = window.originalPostMessage;
+    let originalPostMessage = window.originalPostMessage;
 
-  if (originalPostMessage) {
-    ready();
-  } else {
-    const descriptor: any = {
-      get() {
-        return originalPostMessage;
-      },
-      set(value) {
-        originalPostMessage = value;
-        if (originalPostMessage) {
-          setTimeout(ready, 50);
-        }
-      },
-    };
-    Object.defineProperty(window, 'originalPostMessage', descriptor);
-  }
-  document.addEventListener('FROM_RN', e => {
-    listener(e.data);
-  });
+    if (originalPostMessage) {
+        ready();
+    } else {
+        const descriptor: any = {
+            get() {
+                return originalPostMessage;
+            },
+            set(value) {
+                originalPostMessage = value;
+                if (originalPostMessage) {
+                    setTimeout(ready, 50);
+                }
+            },
+        };
+        Object.defineProperty(window, 'originalPostMessage', descriptor);
+    }
+    document.addEventListener('FROM_RN', e => {
+        listener(e.data);
+    });
 }
 
 export { bind, define, fn, addEventListener, removeEventListener, isConnect };
