49009aa81cef88ab38a5022600d4d9082c83013e
Chengchcc <chen@aion.network>
2019-09-19 17:41:00 +0800

    add search feature

--- app/pages/signed/dex/exchange_token_list.js
+++ app/pages/signed/dex/exchange_token_list.js
@@ -1,13 +1,79 @@
 import * as React from 'react';
-import { View, Dimensions, FlatList, ActivityIndicator, Image, Text, TouchableOpacity } from 'react-native';
+import { View, Dimensions, FlatList, ActivityIndicator, Image, Text, TouchableOpacity, TextInput } from 'react-native';
 import { connect } from 'react-redux';
 import { getTokenIconUrl } from '../../../../client/api';
 import { COINS } from '../../../../client/support_coin_list';
 import { createAction, navigateBack } from '../../../../utils/dva';
+import { strings } from '../../../../locales/i18n';
 
 const { width } = Dimensions.get('window');
 
 class ExchangeTokenList extends React.PureComponent {
+    static navigationOptions = ({ navigation }) => {
+        const keyword = navigation.getParam('keyword', '');
+        const isClearable = navigation.getParam('isClearable', false);
+        const search = navigation.getParam('search', () => {});
+        return {
+            headerTitle: (
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        flex: 1,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <TextInput
+                        style={{
+                            fontSize: 16,
+                            color: 'white',
+                            width: '100%',
+                            paddingLeft: 5,
+                            paddingRight: 5,
+                        }}
+                        value={keyword}
+                        placeholder={strings('select_coin.placeholder_search_token')}
+                        onChangeText={e => {
+                            navigation.setParams({
+                                isClearable: e.length > 0,
+                                keyword: e,
+                            });
+                            search(e);
+                        }}
+                    />
+                </View>
+            ),
+            headerRight: isClearable ? (
+                <TouchableOpacity
+                    onPress={() => {
+                        navigation.setParams({
+                            keyword: '',
+                            isClearable: false,
+                        });
+                        search(e);
+                    }}
+                >
+                    <Image style={{ width: 20, height: 20, marginRight: 20, tintColor: 'white' }} resizeMode="contain" source={require('../../../../assets/icon_clear.png')} />
+                </TouchableOpacity>
+            ) : null,
+        };
+    };
+
+    constructor(props) {
+        super(props);
+        this.props.navigation.setParams({
+            search: this.setKeyword,
+        });
+    }
+
+    state = {
+        keyword: '',
+    };
+
+    setKeyword = keyword => {
+        this.setState({ keyword });
+    };
+
     renderLoading = () => (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <ActivityIndicator animating color="red" size="large" />
@@ -58,11 +124,17 @@ class ExchangeTokenList extends React.PureComponent {
         );
     };
 
-    renderList = data => (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <FlatList style={{ width }} data={data} keyExtractor={(item, index) => `${index}`} renderItem={this.renderItem} />
-        </View>
-    );
+    renderList = data => {
+        const { keyword } = this.state;
+        if (keyword.trim() !== '') {
+            data = data.filter(el => el.symbol.toLowerCase().indexOf(keyword.trim().toLowerCase()) >= 0);
+        }
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <FlatList style={{ width }} data={data} keyExtractor={(item, index) => `${index}`} renderItem={this.renderItem} />
+            </View>
+        );
+    };
 
     render() {
         const { isLoading, tokenList } = this.props;
